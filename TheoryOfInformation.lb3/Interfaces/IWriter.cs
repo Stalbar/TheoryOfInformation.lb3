173f5e979177164feac2ed07430f9eca4926bbe3
Aleksey <aleksey.smorshyok@gmail.com>
2022-04-08 18:24:19 +0300

    add classes and interfaces

---
+++ TheoryOfInformation.lb3/Interfaces/IWriter.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TheoryOfInformation.lb3.Interfaces
+{
+    internal interface IWriter
+    {
+        void WriteToFile(string fileName, byte[] information);
+    }
+}
